84911851c5b791e705308f1291483e8f20b7961d
Andrea <andrea0795@gmail.com>
2021-04-14 20:49:19 -0500

    project finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireAuth, AngularFireAuthModule} from '@angular/fire/auth';
+import firebase from 'firebase/app';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-chat';
+  items: Observable<any[]>;
+  user: any;
+  formValue: any;
+
+  constructor(public firestore: AngularFirestore, public auth: AngularFireAuth) {
+    const query = firestore.collection("messages", ref => {
+      return ref.orderBy("createdAt").limitToLast(30);
+    });
+    this.items = query.valueChanges();
+    this.auth.user.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  sendMessage(){
+
+    this.auth.user.subscribe((user) => {
+
+      this.firestore.collection("messages").add({
+        text: this.formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid : user?.uid,
+        displayName : user?.displayName,
+        photoURL: user?.photoURL
+      });
+
+      this.formValue = '';
+    });
+  }
+
+  signIn() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.auth.user.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  signOut() {
+    this.auth.signOut();
+    this.user = null;
+  }
+
 }
